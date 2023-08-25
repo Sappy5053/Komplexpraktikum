5f9203324af767d8cdc7d1fb0f18c89ec67d7836
Thomas Schmidt <Thomas.Schmidt29@proton.me>
2023-08-25 08:53:43 +0200

    copied "test.py" from "removeStripes" to "myScripts" as "removeStrips.py"

---
+++ myScrips/removeStripes.py
@@ -0,0 +1,62 @@
+import cv2
+import pandas as pd
+import numpy as np
+import os
+
+sep     = '\t'
+erase   = 10
+empty   = '0\t0\t0\t0\t0\n'
+
+    
+#get documents
+path = os.getcwd()
+path1 = path + '/changed/'
+docs = os.listdir(path)
+docs = [doc for doc in docs if doc.endswith('.dat.txt')]
+
+lines1 = []
+for doc in docs:
+    #read documents
+    with open(doc, 'r') as f:
+        lines = f.readlines()
+    
+    for i in range(len(lines)):
+        line = lines[i]
+        l = line.split(sep)
+        if l[2] == '4':
+            #latchup
+            lines1.append(line)
+            for j in range(i+1, i+1+erase):
+##                substitude with empty lines
+                lines[j] = empty
+    file = path1 + doc
+
+    with open(file, 'w') as f:
+        for line in lines1:
+            f.write(line)
+
+    print(len(lines), len(lines1))
+    
+    df = pd.read_csv(path1 + doc, sep = sep, header = None)
+    
+    df.drop(columns = [2], inplace = True)
+
+    df = df.astype(int)
+    df = df/34.4
+    df = df.astype(int)
+##    NP = df.to_numpy()
+
+    arr = np.zeros((120, 120))
+
+##    shape = NP.shape
+##    print(shape)
+
+    for index, row in df.iterrows():
+        x,y = row[0], row[1]
+        arr[x-1,y-1] = 255
+
+    cv2.imwrite(path1 + doc+ '.png', arr)
+    
+
+            
+        
